2811ad8b9ae496dede4afef0d1d701518a1554d8
Christopher Bunting <christophersbunting@gmail.com>
2018-08-12 21:41:37 -0700

    Removes `pony` email sending gem

--- guacamole.rb
+++ guacamole.rb
@@ -3,10 +3,7 @@ require 'pg'
 require 'rack-ssl-enforcer'
 require 'gibbon'
 
-require_relative './email'
 require_relative './models'
-
-welcome_email = ERB.new(IO.read('./views/emails/welcome.html'))
 
 configure :production do
   use Rack::SslEnforcer
