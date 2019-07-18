3a979b7f3a8447e32d1159e642a53bf73c66b76a
Josh Williams <josh.r.williams@gmail.com>
2019-07-17 22:39:52 -0700

    sdasdasd

--- guacamole.rb
+++ guacamole.rb
@@ -25,26 +25,27 @@ end
 
 post '/' do
   enthusiast = GuacamoleEnthusiasts.new(params)
-  if enthusiast.save
-    gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
-    merge_fields = { GUAC: (params[:guac].nil? ? "" : "true"),
-                     BEER: (params[:beer].nil? ? "" : "true"),
-                     FRIENDS: (params[:other].nil? ? "" : "true")
-                   }.reject { |_,v| v.empty? }
-
-    gibbon.lists(ENV['WELCOME_LIST_ID']).members.create(body:
-      { email_address: params[:email],
-        status: "subscribed",
-        merge_fields: merge_fields
-      }
-    )
-    erb :success
-  else
-    erb :home
-  end
-rescue Gibbon::MailChimpError => e
-  print "Oh no, an error occured: #{e}."
   erb :success
+#   if enthusiast.save
+#     gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
+#     merge_fields = { GUAC: (params[:guac].nil? ? "" : "true"),
+#                      BEER: (params[:beer].nil? ? "" : "true"),
+#                      FRIENDS: (params[:other].nil? ? "" : "true")
+#                    }.reject { |_,v| v.empty? }
+
+#     gibbon.lists(ENV['WELCOME_LIST_ID']).members.create(body:
+#       { email_address: params[:email],
+#         status: "subscribed",
+#         merge_fields: merge_fields
+#       }
+#     )
+#     erb :success
+#   else
+#     erb :home
+#   end
+# rescue Gibbon::MailChimpError => e
+#   print "Oh no, an error occured: #{e}."
+#   erb :success
 end
 
 get '/partyon' do
